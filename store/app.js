f2a1490325160ab283f9445cf3948ac2cc9b2bb2
Pablo Moreno <ssm.pablo@gmail.com>
2023-02-03 03:10:55 +0100

    api setup and mongoose schema

--- store/app.js
+++ store/app.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
-//async errors
+require('express-async-errors')
 
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
 //middleware
+
 app.use(express.json())
 
 //routes
@@ -15,6 +18,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 //products routes
 
 app.use(notFoundMiddleware)
@@ -24,7 +29,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
-    //connect db
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`Server is listening in port${port}`))
   } catch (error) {
     console.log(error)
---
+++ store/models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'product name must be provided']
+  },
+  price: {
+    type: String,
+    required: [true, 'product price must be provided']
+  },
+  featured: {
+    type: Boolean,
+    required: [true, 'product name must be provided'],
+    default: false
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['ikea', 'liddy', 'caresma', 'marcos'],
+      message: '{VALUE} is not supported'
+    }
+  }
+})
+
+module.exports = mongoose.model('Product', productSchema)
