7ae240cea374de9c7f7419ef0a3335b223b9b19a
Pablo Moreno <ssm.pablo@gmail.com>
2023-02-04 03:13:42 +0100

    search functionality and filtering/sorting

--- store/controllers/products.js
+++ store/controllers/products.js
@@ -1,19 +1,33 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-  res.status(200).json({ products })
+  const products = await Product.find({}).sort('-name price')
+  res.status(200).json({ products, nHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query
+  const { featured, company, name, sort } = req.query
   const queryObject = {}
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
   }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
   console.log(queryObject)
-  const products = await Product.find(queryObject)
+  let result = Product.find(queryObject)
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  }
+  else {
+    result = result.sort('createdAt')
+  }
+  const products = await result
   res.status(200).json({ products, nHits: products.length })
 }
 
