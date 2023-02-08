f91b9e07e89c7f4cc21ab044d90adbf80cbb286f
Pablo Moreno <ssm.pablo@gmail.com>
2023-02-08 23:53:18 +0100

    refactoring error handling

--- jwt-auth/middleware/auth.js
+++ jwt-auth/middleware/auth.js
@@ -1,21 +1,21 @@
-const CustomAPIError = require('../errors/custom-error')
+const { UnauthenticatedError } = require('../errors')
 const jwt = require('jsonwebtoken')
 
 const authenticationMiddleWare = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer '))
-    throw new CustomAPIError('No token provided', 401)
+    throw new UnauthenticatedError('No token provided')
 
   const token = authHeader.split(' ')[1]
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const {id, username} = decoded
-    req.user = {id, username}
+    const { id, username } = decoded
+    req.user = { id, username }
     next()
   } catch (error) {
-    throw new CustomAPIError('Not authorized to access this route, maybe your token has expired?', 401)
+    throw new UnauthenticatedError('Not authorized to access this route, maybe your token has expired?')
   }
 }
 
