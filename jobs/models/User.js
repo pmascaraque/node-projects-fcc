21a16788221e619a692cd73cb46f0fd792bc386a
Pablo Moreno <ssm.pablo@gmail.com>
2023-02-13 03:22:13 +0100

    login function

--- jobs/models/User.js
+++ jobs/models/User.js
@@ -31,7 +31,12 @@ UserSchema.pre('save', async function () {
 })
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, 'jwtSecret', { expiresIn: '7d' })
+  return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
 }
 
 module.exports = mongoose.model('User', UserSchema)
